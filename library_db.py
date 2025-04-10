ba2834f1749897233cb8762f401e9c2a6719de04
Ilya <iliaha2005@gmail.com>
2025-04-10 10:27:02 +0300

    add: establish connection to db

---
+++ library_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+conn = psycopg2.connect(
+    dbname=db_name,
+    user=db_user,
+    password=db_pass,
+    host=db_host
+)
+print("Connected to the library DB!")
+print(conn)
