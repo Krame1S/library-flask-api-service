8c8b8e2a49b3b2c0461230912c3ec8248ca34a59
Ilya <iliaha2005@gmail.com>
2025-05-19 15:05:20 +0300

    feat: add POST, PUT, and DELETE for /api/v1/books

--- app/db/db.py
+++ app/db/db.py
@@ -33,3 +33,34 @@ def get_all_books():
         result.append(books_dict)
 
     return result
+
+def add_book(isbn, title, copies_available):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO books (isbn, title, copies_available) 
+        VALUES (%s, %s, %s);
+    """, (isbn, title, copies_available))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def update_book(isbn, title, copies_available):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE books
+        SET title = %s, copies_available = %s
+        WHERE isbn = %s;
+    """, (title, copies_available, isbn))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def delete_book(isbn):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM books WHERE isbn = %s;", (isbn,))
+    conn.commit()
+    cursor.close()
+    conn.close()
--- app/services/library_service.py
+++ app/services/library_service.py
@@ -3,3 +3,32 @@ from app.db import db
 def get_books():
     books = db.get_all_books()
     return books
+
+def create_book(isbn, title, copies_available):
+    if not isbn or not title:
+        raise ValueError("ISBN and title are required")
+    if len(title) > 30:
+        raise ValueError("Title must be 30 characters or less")
+    if copies_available < 0:
+        raise ValueError("Copies available cannot be negative")
+    if not isbn.isdigit() or len(isbn) != 13:
+        raise ValueError("ISBN must be 13 digits")
+    db.add_book(isbn, title, copies_available)
+
+def update_book(isbn, title, copies_available):
+    if not isbn or not title:
+        raise ValueError("ISBN and title are required")
+    if len (title) > 30:
+        raise ValueError("Title must be 30 chars or less")
+    if copies_available < 0:
+        raise ValueError("Copies cannot be negative")
+    if not isbn.isdigit() or len(isbn) != 13:
+        raise ValueError("ISBN must be 13 digits")
+    db.update_book(isbn, title, copies_available)    
+
+def delete_book(isbn):
+    if not isbn:
+        raise ValueError("ISBN is required")
+    if not isbn.isdigit() or len(isbn) != 13:
+        raise ValueError("ISBN must be 13 digits")
+    db.delete_book(isbn)
